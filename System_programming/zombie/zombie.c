741df41d6d75d1b9c58917735ed2e701dfc15065
iflei <admin@iflei.com>
2017-02-15 20:34:43 +0800

    僵尸进程产生

---
+++ System_programming/zombie/zombie.c
@@ -0,0 +1,33 @@
+/*************************************************************************
+	> File Name: zombie.c
+	> Author: iflei
+	> Mail: admin@iflei.com
+	> Blog: www.iflei.com
+ ************************************************************************/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+
+int main()
+{
+    pid_t id = fork();
+    if(id == -1)
+    {
+        printf("fork error! errno: %d\n", errno);
+        return 2;
+    }
+    else if(id == 0)
+    {
+        printf("child pid: %d\n", getpid());
+        sleep(3);
+        exit(3);
+    }
+    else
+    {
+        printf("father pid: %d\n", getpid());
+        sleep(20);
+    }
+    return 0;
+}
